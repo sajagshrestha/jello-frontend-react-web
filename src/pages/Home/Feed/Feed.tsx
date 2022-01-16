cbe4a55100a6b456ed879f355c6d9bfa4ee4cc10
Sajag shrestha <sajagshrestha0852@gmail.com>
2022-01-16 10:00:14 +0545

    Add feed title components

--- src/pages/Home/Feed/Feed.tsx
+++ src/pages/Home/Feed/Feed.tsx
@@ -1,7 +1,12 @@
 import { useQuery } from "react-query";
 import PostService from "src/api/services/post-services";
 import ImageCard from "src/pages/common/ImageCard";
-import { FeedContainer, FeedSeparator } from "./Feed.styles";
+import {
+  FeedContainer,
+  FeedSeparator,
+  FeedTitle,
+  FeedTitleSection,
+} from "./Feed.styles";
 
 function Feed() {
   const { data, isLoading } = useQuery("feed", PostService.getFeedPosts);
@@ -11,24 +16,31 @@ function Feed() {
   }
 
   return (
-    <FeedContainer>
-      {data?.map((img) => (
-        <FeedSeparator key={img.id}>
-          <ImageCard
-            id={img.id}
-            likeCount={img.likeCount}
-            liked={img.isLiked}
-            saved={img.isSaved}
-            commentCount={img.comments?.length || 0}
-            caption={img.caption}
-            uploader={img.uploader}
-            url={img.url}
-            tags={img.tags}
-            createdOnDate={img.formatedCreatedOnDate}
-          />
-        </FeedSeparator>
-      ))}
-    </FeedContainer>
+    <>
+      <FeedSeparator>
+        <FeedTitleSection>
+          <FeedTitle>Feed</FeedTitle>
+        </FeedTitleSection>
+      </FeedSeparator>
+      <FeedContainer>
+        {data?.map((img) => (
+          <FeedSeparator key={img.id}>
+            <ImageCard
+              id={img.id}
+              likeCount={img.likeCount}
+              liked={img.isLiked}
+              saved={img.isSaved}
+              commentCount={img.comments?.length || 0}
+              caption={img.caption}
+              uploader={img.uploader}
+              url={img.url}
+              tags={img.tags}
+              createdOnDate={img.formatedCreatedOnDate}
+            />
+          </FeedSeparator>
+        ))}
+      </FeedContainer>
+    </>
   );
 }
 
